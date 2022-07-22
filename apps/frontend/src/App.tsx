771a3dcd254487ae61ee09b3d0020c195efcda65
Ben Smith <ben.smith@startmail.com>
2022-07-23 09:38:42 +1000

    add api test

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,14 @@
 export const App = () => {
+    const makeRequest = () => {
+        fetch(`${import.meta.env.BACKEND_BASE_URL}/test`)
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+    };
+
     return (
         <div>
             <h1>hello</h1>
+            <button onClick={makeRequest}>Click Me!</button>
         </div>
     );
 };
