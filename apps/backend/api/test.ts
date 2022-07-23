98779a331381602d32e3e4c55f9c8d7c61cba631
Ben Smith <ben.smith@startmail.com>
2022-07-23 10:05:35 +1000

    added cors

--- apps/backend/api/test.ts
+++ apps/backend/api/test.ts
@@ -1,6 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { allowCors } from "./_utils";
 
-export default (request: VercelRequest, response: VercelResponse) => {
+const handler = (request: VercelRequest, response: VercelResponse) => {
     const { name } = request.query;
     response.status(200).send(`Hello ${name}!`);
 };
+
+export default allowCors(handler);
