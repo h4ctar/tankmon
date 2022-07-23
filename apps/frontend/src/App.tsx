693665ea222852001984b0bdd8872eba69ba2fe4
Ben Smith <ben.smith@startmail.com>
2022-07-23 10:31:04 +1000

    added tailwindcss

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,15 +1,18 @@
 export const App = () => {
     const makeRequest = () => {
-        console.log(import.meta.env)
         fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/test`)
-            .then((res) => res.json())
+            .then((res) => res.text())
             .then((data) => console.log(data));
     };
 
     return (
-        <div>
-            <h1>hello</h1>
-            <button onClick={makeRequest}>Click Me!</button>
+        <div className="flex h-screen">
+            <div className="m-auto">
+                <h1>hello</h1>
+                <button
+                    className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white"
+                    type="button" onClick={makeRequest}>Click Me!</button>
+            </div>
         </div>
     );
 };
