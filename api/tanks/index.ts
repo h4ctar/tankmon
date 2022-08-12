a73adba6d040ca14ec02e2c4113b4c9fe9d9fd26
Ben Smith <ben.smith@startmail.com>
2022-08-13 09:48:18 +1000

    mock tank response

--- api/tanks/index.ts
+++ api/tanks/index.ts
@@ -1,21 +1,18 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { Tank } from "../src/tank.schema";
-import { allowCors } from "./_utils";
+import type { Tank } from "../../src/tank.schema";
+import { allowCors } from "../_utils";
+
+const TANKS: Tank[] = [{ id: "asdf", name: "Bob" }];
 
 const handler = (request: VercelRequest, response: VercelResponse) => {
     console.log("url", request.url);
     console.log("method", request.method);
     console.log("query", request.query);
     console.log("body", request.body);
-    console.log("body", JSON.parse(request.body));
 
     if (request.method === "GET") {
         console.info("Get all tanks");
-
-        console.log(Tank.parse(JSON.parse(request.body)));
-
-        const { name } = request.query;
-        response.status(200).send(`Hello ${name}!`);
+        response.status(200).send(TANKS);
     } else {
         throw new Error("Unsupported method");
     }
