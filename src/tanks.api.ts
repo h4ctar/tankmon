66606a19c457f27cf04046c20abfd547f0baeb6e
Ben Smith <ben.smith@startmail.com>
2022-08-13 09:43:46 +1000

    base url

--- src/tanks.api.ts
+++ src/tanks.api.ts
@@ -2,14 +2,14 @@ import { Tank } from "./tank.schema";
 
 export const getTanks = async (): Promise<Tank[]> => {
     console.log("Get tanks");
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/tanks`);
+    const response = await fetch("/api/tanks");
     const tanks = await response.json();
     return tanks;
 };
 
 export const getTank = async (id: string): Promise<Tank> => {
     console.log("Get tank");
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/tanks/${id}`);
+    const response = await fetch(`/tanks/${id}`);
     const tank = await response.json();
     return tank;
 };
