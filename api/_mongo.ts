fe97a328218fe9156efe7eecec129a9fa7fe3f1b
Ben Smith <ben.smith@startmail.com>
2022-08-17 20:23:11 +1000

    mongodb

--- api/_mongo.ts
+++ api/_mongo.ts
@@ -1,7 +1,3 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 
-export const client = new MongoClient(process.env.MONGO_URI, {
-    sslKey: process.env.MONGO_CERT,
-    sslCert: process.env.MONGO_CERT,
-    serverApi: ServerApiVersion.v1,
-});
+export const client = new MongoClient(process.env.MONGODB_URI);
