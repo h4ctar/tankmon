5eefccc2dbbdffc1c2ca8ae5de02ee1e76ef2944
Ben Smith <ben.smith@startmail.com>
2023-11-18 17:15:11 +1100

    debug

--- backend/src/status.ts
+++ backend/src/status.ts
@@ -18,10 +18,12 @@ export const statusRoutes: FastifyPluginCallback<
         async (request, reply) => {
             server.log.info("Post status");
 
-            server.log.info("BODY " + request.body);
-            server.log.info("TYPE " + typeof request.body);
+            server.log.info("BODY " + JSON.stringify(request.body));
+            server.log.info("BODY TYPE " + typeof request.body);
 
             const postStatus = request.body as PostStatus;
+            server.log.info("RAW DATA " + JSON.stringify(postStatus.data));
+            server.log.info("RAW DATA TYPE " + typeof postStatus.data);
             const statusData = RawStatus.parse(postStatus.data);
             server.log.info("DATA " + statusData);
             await prisma.status.create({
